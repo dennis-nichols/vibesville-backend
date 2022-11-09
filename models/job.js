a01cf70a7e961f1c5699fb4decc387917702ef47
Dennis Nichols <dgnichols22@gmail.com>
2022-11-09 13:31:25 -0300

    added subscores

--- models/job.js
+++ models/job.js
@@ -16,7 +16,12 @@ const jobSchema = new Schema({
   latitude: { type: String, required: false },
   longitude: { type: String, required: false },
   user_id: { type: String, required: true },
-  user_score: { type: Number, required: false }
+  user_score: { type: Number, required: false },
+  housing_score: { type: Number, required: false},
+  COL_score: { type: Number, required: false},
+  health_score: {type: Number, required: false},
+  nature_score: { type: Number, required: false},
+  culture_score: { type: Number, required: false}
 });
 
 module.exports = mongoose.model('Job', jobSchema);
