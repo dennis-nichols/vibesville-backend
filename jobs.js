7d448b7246821946ab69246e61af8b1c253b88b0
Elias <elias.staehle@gmail.com>
2023-01-05 00:32:45 -0500

    fixing deployment

--- jobs.js
+++ jobs.js
@@ -17,12 +17,12 @@ async function jobsHandler(req, res, next) {
 
       let get_city_data = async (job) => {
         let city = job.city + ', ' + job.state;
-        console.log(city);
+
         let city_url = `https://api.teleport.org/api/cities/?search=${city}&limit=1&embed=city%3Asearch-results%2Fcity%3Aitem%2Fcity%3Aurban_area%2Fua%3Ascores`;
         let city_results = await axios.get(city_url);
         let city_object = city_results.data?._embedded["city:search-results"][0]?._embedded[
-        "city:item"
-      ]?._embedded?.['city:urban_area']?._embedded['ua:scores'];
+          "city:item"
+        ]?._embedded?.['city:urban_area']?._embedded['ua:scores'];
         return city_object;
       }
       let city_data = await get_city_data(job);
@@ -30,7 +30,7 @@ async function jobsHandler(req, res, next) {
     }
 
     // Add city and job data together
-  // job_cities.forEach(city => job_data_send.forEach(job => job.cityHandler(city)));
+    // job_cities.forEach(city => job_data_send.forEach(job => job.cityHandler(city)));
     job_data_send.forEach((job, index) => job.cityHandler(job_cities[index]));
 
     res.status(200).send(job_data_send);
